fb3e8d1f59f1564f2d40701f426fd5193f864095
Jesuseme <61170804+Jesuseme@users.noreply.github.com>
2020-03-25 03:19:43 +0100

    Update python task1.py

--- python task1.py
+++ python task1.py
@@ -1,4 +1,7 @@
+#main function so enable the program repeat after completion
 def main():
+    
+    #the function to make the calculation
     def circle_area(b):
         return 3.1428571429 * (b*b)
  
@@ -6,10 +9,16 @@ def main():
     print("-----------------------------------------")
     print("Area of circle calculator: ")
     print("Enter circle radius:")
+    
+    #this enables user input
     z = int( input())
+    
+    #input is added to the function
     z = circle_area(z)
     print ("Area: " + str(z))
     print("-----------------------------------------")
+    
+    #the main function is called to restart the program
     main()
 
 
